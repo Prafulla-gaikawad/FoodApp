2218cc2c17bbdcedf9f53a9f7fd6baa0378a2769
Prafulla-gaikawad <159248731+Prafulla-gaikawad@users.noreply.github.com>
2025-02-22 01:23:03 +0530

    added

---
+++ backend/db.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+// const MONGO_URL = process.env.MONGO_URL;
+const mongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("connected");
+
+    const fetched_data = await mongoose.connection.db.collection("food_items");
+    const data = await fetched_data.find({}).toArray();
+    const foodCategory = await mongoose.connection.db.collection(
+      "foodCategory"
+    );
+    const catData = await foodCategory.find({}).toArray();
+    global.food_items = data;
+    global.foodCategory = catData;
+  } catch (err) {
+    console.error("--", err);
+  }
+};
+
+module.exports = mongoDB;
---
+++ backend/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = 5050 || process.env.PORT;
+const mongoDB = require("./db");
+mongoDB();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept"
+  );
+  next();
+});
+app.use(express.json());
+app.use("/api", require("./Routes/CreateUser"));
+app.use("/api", require("./Routes/DisplayData"));
+app.use("/api", require("./Routes/OrderData"));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
